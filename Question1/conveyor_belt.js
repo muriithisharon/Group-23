cef8a0493e04e4553330189ccb760312d55a89d7
Mercy Mwikali <mwikalimercykilonzo.com>
2025-05-28 05:54:59 +0300

    changed the branch name

--- Question1/conveyor_belt.js
+++ Question1/conveyor_belt.js
@@ -2,8 +2,7 @@
 function conveyorBelt (item){
     let conveyorlist = ["bag","shoes"]
     conveyorlist.push(item)
-    console.log(conveyorlist);
-    
+     
 
     if (conveyorlist.length > 0){
         dequedItem = conveyorlist.shift()
